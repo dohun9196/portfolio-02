8d99842749c141acd3a7798489be9442d5c2b6c3
Dohun9196 <68097610+dohun9196@users.noreply.github.com>
2022-12-02 17:42:17 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,40 @@
+$(function () {
+    // topBanner
+    $('.topBanner i').on('click', function () {
+        $('.topBanner').addClass('on')
+    });
+    // mainSlide
+    $('.mainSlide').slick({
+        dots: true,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        pasueOnHover: false,
+        pasueOnFocus: false,
+        prevArrow: false,
+        nextArrow: false,
+    });
+
+    $('.main_pro_slide').slick({
+        arrows: true,
+        dots: true,
+        slidesToShow: 3,
+    });
+
+    $('.mainProduct .arrows .prev').on('click', function () {
+        $('.main_pro_slide').slick('slickPrev')
+    });
+
+    $('.mainProduct .arrows .next').on('click', function () {
+        $('.main_pro_slide').slick('slickNext')
+    });
+
+    $('#f_link').on('change', function () {
+        console.log('change', $(this).val());
+        var link = $(this).val();
+
+        if (link) {
+            window.open(link)
+        }
+    })
+
+});
